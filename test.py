cffa88d5e7eaf4050728078ab2317cb322f64721
zhangyx <yx2718@outlook.com>
2022-11-11 02:43:51 +0800

    first add

---
+++ test.py
@@ -0,0 +1,49 @@
+import argparse
+import torch
+import cv2
+from tetris import Tetris
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        """Implementation of Deep Q Network to play Tetris""")
+
+    parser.add_argument("--width", type=int, default=10, help="The common width for all images")
+    parser.add_argument("--height", type=int, default=20, help="The common height for all images")
+    parser.add_argument("--block_size", type=int, default=30, help="Size of a block")
+    parser.add_argument("--fps", type=int, default=300, help="frames per second")
+    parser.add_argument("--saved_path", type=str, default="trained_models")
+    parser.add_argument("--output", type=str, default="output.mp4")
+
+    args = parser.parse_args()
+    return args
+
+
+def test(opt):
+    device = 'mps'
+    torch.manual_seed(123)
+    model = torch.load("{}/tetris".format(opt.saved_path), map_location=lambda storage, loc: storage)
+    model.eval()
+    env = Tetris(width=opt.width, height=opt.height, block_size=opt.block_size)
+    env.reset()
+    model = model.to(device)
+    out = cv2.VideoWriter(opt.output, cv2.VideoWriter_fourcc(*"MJPG"), opt.fps,
+                          (int(1.5 * opt.width * opt.block_size), opt.height * opt.block_size))
+    while True:
+        next_steps = env.get_next_states()
+        next_actions, next_states = zip(*next_steps.items())
+        next_states = torch.stack(next_states)
+        next_states = next_states.to(device)
+        predictions = model(next_states)[:, 0]
+        index = torch.argmax(predictions).item()
+        action = next_actions[index]
+        _, done = env.step(action, render=True, video=out)
+
+        if done:
+            out.release()
+            break
+
+
+if __name__ == "__main__":
+    opt = get_args()
+    test(opt)
